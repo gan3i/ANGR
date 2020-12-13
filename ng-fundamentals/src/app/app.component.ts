7f7a1318be086c9a896a8aec85b42326a48ec5ee
Gan3i <gani468@gmail.com>
2020-12-13 22:41:00 +0530

    hello world!

--- ng-fundamentals/src/app/app.component.ts
+++ ng-fundamentals/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'events-app',
+  template: '<h1>Hello! world</h1>'
 })
 export class AppComponent {
   title = 'ng-fundamentals';
