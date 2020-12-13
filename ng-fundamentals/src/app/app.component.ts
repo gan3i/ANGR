8c8019213f0865a7e8eda66616215f62340b3bee
Gan3i <gani468@gmail.com>
2020-12-13 23:23:46 +0530

    ngf-bootstrap and images added

--- ng-fundamentals/src/app/app.component.ts
+++ ng-fundamentals/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<h1>Hello! world</h1>'
+  template: '<h1>Hello! world</h1><img src = "./assets/images/basic-shield.png"/>'
 })
 export class AppComponent {
   title = 'ng-fundamentals';
